47a17e2d5b72f6bdd25be5542122509cb89829b6
Munyua123 <earlmunyua96@gmail.com>
2025-05-03 09:42:28 +0300

    Added the models, created the database and tables

---
+++ backend/app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.routes import main
+    app.register_blueprint(main)
+
+    return app
---
+++ backend/app/models.py
@@ -0,0 +1,82 @@
+from . import db
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.schema import CheckConstraint
+
+class Book(db.Model,SerializerMixin):
+    __tablename__ = 'books'
+    
+    serialize_rules = ('-orders.book',)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(255), nullable=False)
+    book_price = db.Column(db.Integer, nullable=False)
+    book_author = db.Column(db.String(255), nullable=False)
+    book_genre = db.Column(db.String(255), nullable=False)
+    book_stock = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now()) 
+    
+    orders = db.relationship('Orders', back_populates='book')
+    
+    __table_args__=(
+        CheckConstraint(
+            'book_stock >= 0',
+            name= 'stock_check'
+        ),
+        CheckConstraint(
+            'book_price >= 0',
+            name= 'price_check'
+        ),
+    )
+    
+    
+class Customer(db.Model,SerializerMixin):
+    __tablename__ = 'customers'
+    
+    serialize_rules = ('-orders.customer',)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    customer_name = db.Column(db.String(255), nullable=False)
+    phone_number = db.Column(db.String(10), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    orders = db.relationship('Orders', back_populates='customer')
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(str(phone_number)) != 10:
+            raise ValueError("Phone number must be 10 digits")
+        return phone_number 
+    
+    
+class Order(db.Model,SerializerMixin):
+    __tablename__ = 'orders'
+    
+    serialize_rules = ('-customer.orders', '-book.orders')
+    
+    id = db.Column(db.Integer, primary_key=True)
+    order_number = db.Column(db.Integer, unique=True, nullable=False)
+    date_issued = db.Column(db.DateTime, server_default=db.func.now())
+    return_date = db.Column(db.DateTime)
+    rent_fee = db.Column(db.Float)
+    returned = db.Column(db.Boolean, default=False)
+    
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    
+    customer = db.relationship('Customer', back_populates='orders')
+    book = db.relationship('Book', back_populates='orders')
+    
+    __table_args__=(
+        CheckConstraint(
+            'rent_fee >= 0',
+            name= 'rent_fee_check'
+        ),
+        CheckConstraint(
+            'return_date >= date_issued',
+            name= 'return_date_check'
+        )
+    )
+    
